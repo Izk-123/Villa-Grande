5a39ba3546e7203e05af5ad2b642171befed7312
Izk-123 <109396886+Izk-123@users.noreply.github.com>
2026-04-11 16:52:58 +0200

    Update settings.py

--- villagrande/settings.py
+++ villagrande/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'villagrande.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'villagrande_db',
+        'USER': 'villagrande_user',
+        'PASSWORD': 'Malawi@2026',   # use the password you set
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
